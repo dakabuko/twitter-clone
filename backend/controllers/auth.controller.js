d0214dbee5af5bf7c5acc07163386a0d61a913eb
dakabuko <bgmahbub07@gmail.com>
2025-12-16 15:24:45 +0600

    1-auth controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,107 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "You hit the signup endpoint",
-  });
+  try {
+    const { fullname, username, email, password } = req.body;
+
+    if (!fullname || !username || !email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 charecters long" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    generateTokenAndSetCookie(newUser._id, res);
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullname: newUser.fullname,
+      username: newUser.username,
+      email: newUser.email,
+    });
+  } catch (error) {
+    console.log("Error in signup controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login endpoint",
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCOrrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCOrrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+  }
 };
+
 export const logout = async (req, res) => {
-  res.json({
-    data: "You hit the logout endpoint",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getMe controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -8,6 +9,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // ✅ ADD THIS LINE
 
 app.use("/api/auth", authRoute);
 
